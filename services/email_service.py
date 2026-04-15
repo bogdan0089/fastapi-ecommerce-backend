265cc663d199978f23cc134c02b5ce6a3d83dc0f
bohdan0089 <bodiks18@gmail.com>
2026-04-15 17:58:53 +0200

    feat: add email verification with token and smtp service

--- services/email_service.py
+++ services/email_service.py
@@ -8,7 +8,7 @@ class EmailService:
 
     @staticmethod
     async def send_verification_email(to_email: str, token: str):
-        message = MIMEText(f"code: {token}")
+        message = MIMEText(f"Click to verify your email:\nhttp://localhost:8000/auth/verify/{token}")
         message["Subject"] = "Verification email"
         message["From"] = settings.EMAIL_USER
         message["To"] = to_email
